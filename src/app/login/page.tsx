fc8b879a26f6e2a844d9bd42c69a47a43e8537f8
mizunatoma <k40117135@gmail.com>
2025-11-03 11:21:25 +0900

    ログイン状態に応じてログイン/ログアウトボタンを切り替え

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
   }
 
   return (
-    <div className="flex justify-center pt-[240px]">
+    <div className="flex justify-center pt-[120px]">
       <form onSubmit={handleSubmit} className="space-y-4 w-full max-w-[400px]">
         <div>
           <label
